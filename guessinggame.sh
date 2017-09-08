0bb1008d575f7b894203c781aeddf1ef5d59778c
Oliver1961 <31265903+Oliver1961@users.noreply.github.com>
2017-09-08 14:26:44 -0700

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,20 +4,19 @@
 echo "Guess how many files are in the current directory"
 read guess 
 echo "you entered: $guess"
+
 count=(*)
 count=${#count[@]}
+
 let diff=$guess-$count
-while [[ $diff -ne 0 ]]
-do 
-    if [[ $diff -lt 0 ]]
+
+if [[ $diff -lt 0 ]]
     then
         echo "Too low; try again"
     elif [[ $diff -gt 0 ]]
     then
         echo "Too high; try again"
-fi 
-done
-if [[ $diff -eq 0 ]]
-then 
-    echo "Congratulations! You guessed correctly; there are $count files in the current directory!"
+    elif [[ $diff -eq 0 ]]
+    then 
+        echo "Congratulations! You guessed correctly; there are $count files in the current directory!"
 fi 
